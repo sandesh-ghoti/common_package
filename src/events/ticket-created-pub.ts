d49661491ccda0bef01633d49155d6a9adf3f9ca
sandesh ghoti <sandesh.ghoti@gmail.com>
2024-07-12 18:21:56 +0000

    Updates

--- src/events/ticket-created-pub.ts
+++
@@ -1,7 +0,0 @@
-import { Publisher } from "./base-pub";
-import { Subjects } from "./shared";
-import { TicketCreatedEvent } from "./ticket-created-events";
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-  readonly subject: Subjects.TICKET_CREATED = Subjects.TICKET_CREATED;
-}
